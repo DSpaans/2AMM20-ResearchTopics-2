56c63da6908376a539bcd0834b5ed607eb54268f
DSpaans <d.c.spaans@student.tue.nl>
2025-10-13 17:22:05 +0200

    oops

--- chargax-submission/chargax/environment/_data_loaders.py
+++ chargax-submission/chargax/environment/_data_loaders.py
@@ -1,17 +1,15 @@
-import csv
 from importlib import resources as r
-from typing import Literal
-
-import jax.numpy as jnp
 import numpy as np
+import csv
+from typing import Literal
+import jax.numpy as jnp
 
 DATA_FOLDER = "data"
 
-
 def _average_data(data, length):
-    """
-    Average over data to a desired length
-    i.e. array([0, 5, 10]) -> array([0, 2.5, 2.5, 5, 5]) if length = 5
+    """ 
+        Average over data to a desired length 
+        i.e. array([0, 5, 10]) -> array([0, 2.5, 2.5, 5, 5]) if length = 5
     """
     # x = np.array(data)
     x = jnp.array(data)
@@ -21,28 +19,34 @@ def _average_data(data, length):
     x = x.flatten()
     return jnp.array(x)
 
-
 def _interpolate_data_linear(data, length):
-    """Interpolates data to a desired length using linear interpolation"""
+    """ Interpolates data to a desired length using linear interpolation """
     x = np.linspace(0, len(data) - 1, num=len(data))
     x_new = np.linspace(0, len(data) - 1, num=length)
     return np.interp(x_new, x, data)
 
-
 def _interpolate_data_stepwise(data, length):
-    """
-    Interpolates data to a desired length using stepwise interpolation
-    new values are set to the previous value
+    """ 
+        Interpolates data to a desired length using stepwise interpolation 
+        new values are set to the previous value
     """
     x = np.array(data)
     x = np.repeat(x, length // x.shape[1], axis=1)
     assert x.shape[1] == length and x.shape[0] == len(data)
     return x
 
+# def get_scenario(dataset: str, minutes_per_timestep: int = 5):
+#     resources = r.files("chargax")
+#     if dataset == "office":
+#         csv_file = resources.joinpath(DATA_FOLDER, "office_distribution_means.csv")
+#     with open(csv_file, "r") as f:
+#         reader = csv.reader(f)
+#         data = [float(row[1]) for row in list(reader)[1:]]
+#     desired_length = 24 * 60 // minutes_per_timestep
+#     return _average_data(data, desired_length)
+#     return _interpolate_data_linear(data, desired_length)
 
-def get_scenario(
-    dataset: str, average_cars_per_day: int = 100, minutes_per_timestep: int = 5
-):
+def get_scenario(dataset: str, average_cars_per_day: int = 100, minutes_per_timestep: int = 5):
     def _load_scenario_data(csv_file):
         with open(csv_file, "r") as f:
             reader = csv.DictReader(f)
@@ -57,21 +61,14 @@ def get_scenario(
         "car_arrival_percentages_workdays.csv",
         "car_arrival_percentages_weekends.csv",
         "car_connection_times.csv",
-        "car_energy_demand.csv",
+        "car_energy_demand.csv"
     ]
-    data = [
-        _load_scenario_data(resources.joinpath(DATA_FOLDER, file)) for file in csv_files
-    ]
+    data = [_load_scenario_data(resources.joinpath(DATA_FOLDER, file)) for file in csv_files]
     desired_length = 24 * 60 // minutes_per_timestep
-    data[0] = (
-        _average_data(data[0], desired_length) / 100
-    ) * average_cars_per_day  # data is in percentages (0-100) --> make it absolute
-    data[1] = (
-        _average_data(data[1], desired_length) / 100
-    ) * average_cars_per_day  # data is in percentages (0-100) --> make it absolute
-    data[2] = data[2] * 60  # convert hours to minutes
+    data[0] = (_average_data(data[0], desired_length) / 100) * average_cars_per_day # data is in percentages (0-100) --> make it absolute
+    data[1] = (_average_data(data[1], desired_length) / 100) * average_cars_per_day # data is in percentages (0-100) --> make it absolute
+    data[2] = data[2] * 60 # convert hours to minutes
     return tuple(data)
-
 
 def get_electricity_prices(dataset: str = "2023_NL", minutes_per_timestep: int = 5):
     resources = r.files("chargax")
@@ -81,7 +78,6 @@ def get_electricity_prices(dataset: str = "2023_NL", minutes_per_timestep: int =
         data = [list(map(float, row[1:])) for row in list(reader)[1:]]
     desired_length = 24 * 60 // minutes_per_timestep
     return _interpolate_data_stepwise(data, desired_length)
-
 
 def get_car_data(dataset: Literal["eu", "us", "world"]):
     resources = r.files("chargax")
